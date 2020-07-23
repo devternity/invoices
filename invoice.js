1c8ff327c6042f30c207f8e5e1aebf015a160805
Eduards Sizovs <eduards@devternity.com>
2020-07-23 22:15:22 +0700

    add devcapital support

--- invoice.js
+++ invoice.js
@@ -8,6 +8,21 @@ angular.module('app', ['ngRoute'])
             if (data.paymentMethod === 'VISA') {
               data.paymentLink = 'https://api.devternity.com/invoice/' + invoiceUUID + '/linkpay'
             }
+            if (data.billTo === 'devcapital') {
+              data.billTo = {};
+              data.billTo.name = 'DevCapital OÜ';
+              data.billTo.regNo = '12194848';
+              data.billTo.vatNo = '-';
+              data.billTo.address = 'J. Koorti tn 16-20 Tallinn, Estonia';
+              data.billTo.iban = 'EE497700771004301717';
+            } else {
+              data.billTo = {};
+              data.billTo.name = 'DevTernity OÜ';
+              data.billTo.regNo = '14226860'
+              data.billTo.vatNo = 'EE101974721'
+              data.billTo.address = 'Mõisavahe 38-129 Tartu, Estonia';
+              data.billTo.iban = 'EE757700771002512472';
+            }
             return data;
           });
       };
