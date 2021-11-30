7e2e7de418242d462e8538dc3919c89bf0a0966a
Eduards Sizovs <eduards.sizovs@gmail.com>
2021-11-30 19:16:28 +0700

    make payment link overridable

--- invoice.js
+++ invoice.js
@@ -5,7 +5,7 @@ angular.module('app', ['ngRoute'])
           .get("https://devternity-22e74.firebaseio.com/invoices/" + invoiceUUID + ".json")
           .then(function(it) {
             const data = it.data;
-            data.paymentLink = 'https://api.devternity.com/invoice/' + invoiceUUID + '/linkpay'
+            data.paymentLink = data.paymentLink || 'https://api.devternity.com/invoice/' + invoiceUUID + '/linkpay'
             if (data.billTo === 'devcapital') {
               data.billTo = {};
               data.billTo.name = 'DevCapital OÜ';
