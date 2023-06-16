61f89ace74304671c7e5c31eb2046858d0f8e114
Eduards Sizovs <eduards@devternity.com>
2023-06-16 16:03:09 +0800

    uncache invoices

--- invoice.js
+++ invoice.js
@@ -1,66 +1,66 @@
 angular.module('app', ['ngRoute'])
-  .service('Invoice', function($http) {
-      this.fetch = function (invoiceUUID) {
-        return $http
-          .get("https://devternity-22e74.firebaseio.com/invoices/" + invoiceUUID + ".json")
-          .then(function(it) {
-            const data = it.data;
-            data.invoiceUUID = invoiceUUID;
-            if (data.billTo === 'devcapital') {
-              data.billTo = {};
-              data.billTo.name = 'DevCapital OÜ';
-              data.billTo.regNo = '12194848';
-              data.billTo.vatNo = '-';
-              data.billTo.address = 'J. Koorti tn 16-20 Tallinn, Estonia';
-              data.billTo.iban = 'EE497700771004301717';
-            } else {
-              data.billTo = {};
-              data.billTo.name = 'DevTernity OÜ';
-              data.billTo.regNo = '14226860'
-              data.billTo.vatNo = 'EE101974721'
-              data.billTo.address = 'Mõisavahe 38-129 Tartu, Estonia, 50708';
-              data.billTo.iban = 'EE757700771002512472';
-            }
-            return data;
-          });
-      };
-})
-
-.config(function($routeProvider) {
-  var fetchInvoice = {
-    invoice: function (Invoice, $route, $location) {
-      return Invoice
-        .fetch($route.current.params.invoiceUUID)
-        .then(function(it) {
-          if (!it) {
-            $location.path("/")
+  .service('Invoice', function ($http) {
+    this.fetch = function (invoiceUUID) {
+      return $http
+        .get("https://devternity-22e74.firebaseio.com/invoices/" + invoiceUUID + ".json")
+        .then(function (it) {
+          const data = it.data;
+          data.invoiceUUID = invoiceUUID;
+          if (data.billTo === 'devcapital') {
+            data.billTo = {};
+            data.billTo.name = 'DevCapital OÜ';
+            data.billTo.regNo = '12194848';
+            data.billTo.vatNo = '-';
+            data.billTo.address = 'J. Koorti tn 16-20 Tallinn, Estonia';
+            data.billTo.iban = 'EE497700771004301717';
+          } else {
+            data.billTo = {};
+            data.billTo.name = 'DevTernity OÜ';
+            data.billTo.regNo = '14226860'
+            data.billTo.vatNo = 'EE101974721'
+            data.billTo.address = 'Mõisavahe 38-129 Tartu, Estonia, 50708';
+            data.billTo.iban = 'EE757700771002512472';
           }
-          return it;
-        })
-    }
-  };
+          return data;
+        });
+    };
+  })
 
-  $routeProvider
-    .when('/', {
-      templateUrl: 'missing.html'
-    })
-    .when('/:invoiceUUID', {
-      controller:'ViewInvoice as invoice',
-      templateUrl: 'render.html',
-      resolve: fetchInvoice
-    })
-    .otherwise({
-      redirectTo:'/'
-    });
-})
+  .config(function ($routeProvider) {
+    var fetchInvoice = {
+      invoice: function (Invoice, $route, $location) {
+        return Invoice
+          .fetch($route.current.params.invoiceUUID)
+          .then(function (it) {
+            if (!it) {
+              $location.path("/")
+            }
+            return it;
+          })
+      }
+    };
 
-.controller('ViewInvoice', function($location, $routeParams, invoice) {    
+    $routeProvider
+      .when('/', {
+        templateUrl: 'missing.html'
+      })
+      .when('/:invoiceUUID', {
+        controller: 'ViewInvoice as invoice',
+        templateUrl: 'renderz.html',
+        resolve: fetchInvoice
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  })
+
+  .controller('ViewInvoice', function ($location, $routeParams, invoice) {
     if (!invoice) {
       return;
     }
     this.issued = moment(invoice.issued).format("MMMM DD, YYYY");
     this.due = moment(invoice.issued).add(invoice.dueDays, 'days').format("MMMM DD, YYYY");
     this.details = invoice;
-    this.currencyChar = invoice.currencyChar || "€" 
-    this.currency = invoice.currency || "EUR" 
-});
+    this.currencyChar = invoice.currencyChar || "€"
+    this.currency = invoice.currency || "EUR"
+  });
